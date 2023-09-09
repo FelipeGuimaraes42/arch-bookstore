1a8861ef9dbd2999b397bc88e06602626bafb010
Felipe Guimaraes <felipeguima4@gmail.com>
2023-09-09 12:30:40 -0300

    More E2E testing

--- web/cypress/e2e/arch-bookstore.cy.ts
+++ web/cypress/e2e/arch-bookstore.cy.ts
@@ -1,17 +1,48 @@
+/// <reference types="cypress" />
+
 describe('Arch Bookstore', () => {
   it('It should create a new account and log on it', () => {
+    // Opens application
     cy.visit('http://localhost:5173');
 
+    // Creates an account
     cy.get('a').contains('SIGN UP').click();
     cy.get('input[type="text"]').type('testeCypress');
     cy.get('input[type="password"]').type('testeCypress');
     cy.get('button').contains('Create').click();
+    // cy.contains('div', 'Successfully saved!')
 
+    // Navigates to sign in screen
     cy.get('a').contains('Already have an account? Sign In').click();
     cy.wait(1000);
 
+    // Sign in with the created account
     cy.get('input[type="text"]').type('testeCypress');
     cy.get('input[type="password"]').type('testeCypress');
     cy.get('button').contains('Sign In').click();
+    
+    // Verifies if login was successful
+    cy.wait(1000);
+    cy.get('button').contains('LOGOUT')
+
+    // Add a book to the cart
+    cy.contains('.card', 'Engenharia de Software Moderna').find('button').contains('Add to cart').click()
+
+    // Verifies cart icons
+    cy.get('.dWuAmA').click()
+    cy.contains('h2', 'Shopping Cart')
+    cy.contains('h3', 'Engenharia de Software Moderna')
+    cy.contains('button', '-')
+    cy.contains('button', '+')
+    cy.contains('p', '1')
+    
+    cy.contains('button', '+').click()
+    cy.contains('p', '2')
+    
+    cy.contains('button', '-').click()
+    cy.contains('p', '1')
+    
+    cy.contains('button', '-').click()
+    cy.contains('p', 'No items in cart.')
   })
 })
