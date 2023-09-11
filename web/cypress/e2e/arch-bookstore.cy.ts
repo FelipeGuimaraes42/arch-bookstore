9622c5585ec6005831993990a37ef57a0a2411a6
Felipe Guimaraes <felipeguima4@gmail.com>
2023-09-11 20:10:34 -0300

    more e2e tests

--- web/cypress/e2e/arch-bookstore.cy.ts
+++ web/cypress/e2e/arch-bookstore.cy.ts
@@ -8,6 +8,10 @@ describe('Arch Bookstore', () => {
     // Opens application
     cy.visit('http://localhost:5173');
 
+    // Verify buttons
+    cy.contains('a', 'SIGN IN').should('exist');
+    cy.contains('a', 'SIGN UP').should('exist');
+
     // Redirects user to login screen if it tries to add a book to cart without being logged in
     cy.contains('.card', 'Engenharia de Software Moderna').find('button').contains('Add to cart').click();
     cy.wait(500);
@@ -69,15 +73,56 @@ describe('Arch Bookstore', () => {
     cy.contains('button', '-');
     cy.contains('button', '+');
     cy.contains('p', '1');
+    cy.contains('p', 'Price: R$67.60');
+    cy.contains('p', 'Total: R$67.60');
+
+    cy.contains('h2', 'Total: R$67.60');
     
     cy.contains('button', '+').click();
     cy.contains('p', '2');
+    cy.contains('p', 'Price: R$67.60');
+    cy.contains('p', 'Total: R$135.20');
+
+    cy.contains('h2', 'Total: R$135.20');
     
     cy.contains('button', '-').click();
     cy.contains('p', '1');
+    cy.contains('p', 'Price: R$67.60');
+    cy.contains('p', 'Total: R$67.60');
+
+    cy.contains('h2', 'Total: R$67.60');
     
     cy.contains('button', '-').click();
     cy.contains('p', 'No items in cart.');
+    cy.contains('h2', 'Total: R$0.00');
+    
+    // Leave cart screen
+    cy.get('body').click(1, 1);
+
+    // Add two different books
+    cy.contains('.card', 'The Hitchhiker\'s Guide to the Galaxy').find('button').contains('Add to cart').click();
+    cy.contains('.card', 'The Hobbit').find('button').contains('Add to cart').click();
+
+    cy.get('.dWuAmA').click();
+    cy.contains('h3', 'The Hitchhiker\'s Guide to the Galaxy');
+    cy.contains('h3', 'The Hobbit');
+    
+    cy.contains('p', 'Price: R$14.99');
+    cy.contains('p', 'Total: R$14.99');
+    cy.contains('p', 'Price: R$19.99');
+    cy.contains('p', 'Total: R$19.99');
+
+    cy.contains('h2', 'Total: R$34.98');
+
+    cy.contains('.fQxOdQ', 'The Hitchhiker\'s Guide to the Galaxy').find('button').contains('-').click();
+    cy.contains('p', 'Price: R$19.99');
+    cy.contains('p', 'Total: R$19.99');
+
+    cy.contains('h2', 'Total: R$19.99');
+
+    cy.contains('.fQxOdQ', 'The Hobbit').find('button').contains('-').click();
+    cy.contains('p', 'No items in cart.');
+    cy.contains('h2', 'Total: R$0.00');
     
     // Leave cart screen
     cy.get('body').click(1, 1);
